d87fcc51b9077a124e113364cf47116e10043ae9
namung08 <namung08@gmail.com>
2024-06-28 13:20:14 +0900

    del: dont use file del

--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import Router from "koa-router";
-import userController from "../controllers/userController.js";
-
-const router = new Router();
-
-router.get("/", userController.list);
-router.get("/:id", userController.get);
-
-export default router;
