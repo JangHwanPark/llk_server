49a78ca4a5af383898e570f25be31edaf3740389
namung08 <namung08@gmail.com>
2024-06-28 13:21:24 +0900

    feat: add default spring boot settings

---
+++ src/main/java/com/real_estate/llk_server_spring/LlkServerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.real_estate.llk_server_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlkServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlkServerSpringApplication.class, args);
+    }
+
+}
