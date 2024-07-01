ddca2736637faad6068488db32cc27f22d0c30b9
namung08 <namung08@gmail.com>
2024-07-01 22:46:46 +0900

    pref: Entity to contain refresh token

---
+++ src/main/java/com/real_estate/llk_server_spring/security/entity/RefreshRepository.java
@@ -0,0 +1,6 @@
+package com.real_estate.llk_server_spring.security.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshRepository extends JpaRepository<Refresh, Long> {
+}
