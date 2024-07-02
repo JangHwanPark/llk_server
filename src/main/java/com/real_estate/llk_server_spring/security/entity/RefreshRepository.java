2e64466acca927be865f1801895b784d3a4662e3
namung08 <namung08@gmail.com>
2024-07-02 21:51:25 +0900

    feat: Add logout logic

--- src/main/java/com/real_estate/llk_server_spring/security/entity/RefreshRepository.java
+++ src/main/java/com/real_estate/llk_server_spring/security/entity/RefreshRepository.java
@@ -1,9 +1,10 @@
 package com.real_estate.llk_server_spring.security.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface RefreshRepository extends JpaRepository<Refresh, Long> {
     boolean existsByRefresh(String refresh);
-
-    long deleteByRefresh(String refresh);
+    @Transactional
+    void deleteByRefresh(String refresh);
 }
