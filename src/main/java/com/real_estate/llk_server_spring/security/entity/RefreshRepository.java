8eb302c6e67546630b77082e06ed5491345d7c7f
namung08 <namung08@gmail.com>
2024-07-02 20:29:26 +0900

    feat: Controller for reissuing access tokens and refresh tokens

--- src/main/java/com/real_estate/llk_server_spring/security/entity/RefreshRepository.java
+++ src/main/java/com/real_estate/llk_server_spring/security/entity/RefreshRepository.java
@@ -3,4 +3,7 @@ package com.real_estate.llk_server_spring.security.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshRepository extends JpaRepository<Refresh, Long> {
+    boolean existsByRefresh(String refresh);
+
+    long deleteByRefresh(String refresh);
 }
