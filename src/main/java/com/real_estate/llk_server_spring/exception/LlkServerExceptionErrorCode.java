86334175c6171a312bf3940706f4f7b83b7a198c
namung08 <namung08@gmail.com>
2024-07-15 15:18:08 +0900

    fix: Fix server not turning on error due to @NoArgsConstructor

--- src/main/java/com/real_estate/llk_server_spring/exception/LlkServerExceptionErrorCode.java
+++ src/main/java/com/real_estate/llk_server_spring/exception/LlkServerExceptionErrorCode.java
@@ -2,12 +2,10 @@ package com.real_estate.llk_server_spring.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-@NoArgsConstructor
 @AllArgsConstructor
 public enum LlkServerExceptionErrorCode {
     NOT_FOUNT_AGENT("001_LLK","Real estate agent information not found.");
