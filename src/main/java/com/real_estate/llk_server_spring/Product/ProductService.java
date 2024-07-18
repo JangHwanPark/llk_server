f5eac8870fa71aed5e7697acdb30d3ae42b05571
namung08 <namung08@gmail.com>
2024-07-18 20:43:31 +0900

    feat: Added real estate logistics registration function

--- src/main/java/com/real_estate/llk_server_spring/Product/ProductService.java
+++ src/main/java/com/real_estate/llk_server_spring/Product/ProductService.java
@@ -1,33 +1,66 @@
 package com.real_estate.llk_server_spring.Product;
 
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.real_estate.llk_server_spring.Product.Entity.Product;
 import com.real_estate.llk_server_spring.Product.Entity.ProductRepository;
-import com.real_estate.llk_server_spring.Product.Entity.ProductSale;
-import com.real_estate.llk_server_spring.Product.Entity.ProductType;
 import com.real_estate.llk_server_spring.Product.dto.ProjectDTO;
+import com.real_estate.llk_server_spring.exception.LlkServerException;
+import com.real_estate.llk_server_spring.util.LlkUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final LlkUtil llkUtil;
+    private final AmazonS3Client amazonS3Client;
+    @Value("${s3.bucket-name}")
+    private String bucketName;
 
-    public Product createProduct(ProjectDTO projectDTO) {
-        Product product = new Product();
-        product.setProductName(projectDTO.getName());
-        product.setProductDescription(projectDTO.getDescription());
-        product.setProductPrice(projectDTO.getPrice());
-        product.setProductAdd(projectDTO.getAdd());
-        product.setProductType(ProductType.APARTMENT);
-        product.setProductArea(projectDTO.getArea());
-        product.setProductRoom(projectDTO.getRoom());
-        product.setProductBathroom(projectDTO.getBathroom());
-        product.setProductYear(projectDTO.getYear());
-        product.setProductImg(projectDTO.getImg());
-        product.setProductReg(projectDTO.getReg());
-        product.setProductCor(projectDTO.getCor());
-        product.setProductSale(ProductSale.SALE);
-        return productRepository.save(product);
+    public ResponseEntity<?> createProductProc(ProjectDTO projectDTO, List<MultipartFile> files) {
+        try {
+            llkUtil.usingStringDataValidationCheck(projectDTO.getName());
+            llkUtil.usingStringDataValidationCheck(projectDTO.getDescription());
+            llkUtil.usingStringDataValidationCheck(String.valueOf(projectDTO.getPrice()));
+            llkUtil.usingStringDataValidationCheck(projectDTO.getAdd());
+            llkUtil.usingStringDataValidationCheck(String.valueOf(projectDTO.getType()));
+            llkUtil.usingStringDataValidationCheck(String.valueOf(projectDTO.getArea()));
+            llkUtil.usingStringDataValidationCheck(String.valueOf(projectDTO.getRoom()));
+            llkUtil.usingStringDataValidationCheck(String.valueOf(projectDTO.getBathroom()));
+            llkUtil.usingStringDataValidationCheck(String.valueOf(projectDTO.getYear()));
+            llkUtil.usingStringDataValidationCheck(String.valueOf(projectDTO.getSale()));
+            llkUtil.usingStringDataValidationCheck(files.toString());
+        } catch (LlkServerException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        // img file upload logic
+        Product product = new Product().builder()
+                .productName(projectDTO.getName())
+                .productDescription(projectDTO.getDescription())
+                .productPrice(BigDecimal.valueOf(projectDTO.getPrice()))
+                .productAdd(projectDTO.getAdd())
+                .productType(projectDTO.getType())
+                .productArea(BigDecimal.valueOf(projectDTO.getArea()))
+                .productRoom(projectDTO.getRoom())
+                .productBathroom(projectDTO.getBathroom())
+                .productYear(projectDTO.getYear())
+                .productSale(projectDTO.getSale())
+                .build();
+        productRepository.save(product);
+        llkUtil.imgFileUploadProc(files,product,"hojung");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("product created successfully");
+    }
+
+    public ResponseEntity<?> deleteProductProc(String id) {
+        return null;
     }
 }
