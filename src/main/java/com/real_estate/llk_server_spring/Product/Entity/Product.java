5dac5f81541d756975b2707603bdc6125b3f6749
namung08 <namung08@gmail.com>
2024-07-18 20:29:09 +0900

    perf: Real estate logistics table and photo link table settings for each logistics

--- src/main/java/com/real_estate/llk_server_spring/Product/Entity/Product.java
+++ src/main/java/com/real_estate/llk_server_spring/Product/Entity/Product.java
@@ -3,18 +3,19 @@ package com.real_estate.llk_server_spring.Product.Entity;
 import com.real_estate.llk_server_spring.user.entity.Agent;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "product", schema = "db2451506_llk")
+@NoArgsConstructor @AllArgsConstructor @Builder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,47 +23,49 @@ public class Product {
     private Long id;
 
     @NotBlank
-    @Column(name = "product_name")
+    @Column(name = "product_name", nullable = false)
     private String productName;
 
-    @Column(name = "product_description")
+    @Column(name = "product_description", nullable = false)
     private String productDescription;
 
-    @Column(name = "product_price")
+    @Column(name = "product_price", nullable = false, scale = 2)
     private BigDecimal productPrice;
 
     @NotBlank
-    @Column(name = "product_add")
+    @Column(name = "product_add", nullable = false)
     private String productAdd;
 
-    @Column(name = "product_type")
+    @Column(name = "product_type", nullable = false)
     @Enumerated(EnumType.STRING)
     private ProductType productType;
 
-    @Column(name = "product_area")
+    @Column(name = "product_area", scale = 1, precision = 10, nullable = false)
     private BigDecimal productArea;
 
-    @Column(name = "product_room")
+    @Column(name = "product_room", nullable = false)
     private Integer productRoom;
 
-    @Column(name = "product_bathroom")
+    @Column(name = "product_bathroom", nullable = false)
     private Integer productBathroom;
 
-    @Column(name = "product_year")
+    @Column(name = "product_year", nullable = false)
     private LocalDate productYear;
 
-    @Column(name = "product_img")
-    private String productImg;
-
-    @Column(name = "product_reg")
+    @Column(name = "product_reg", nullable = false, updatable = false)
+    @CreationTimestamp
     private LocalDate productReg;
 
-    @Column(name = "product_cor")
+    @Column(name = "product_cor", nullable = false)
+    @UpdateTimestamp
     private LocalDate productCor;
 
-    @Column(name = "product_sale")
+    @Column(name = "product_sale", nullable = false)
     @Enumerated(EnumType.STRING)
     private ProductSale productSale;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductImgLinkList> productImgLinkList;
 
     @JoinColumn(name = "agent_id")
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/real_estate/llk_server_spring/Product/Entity/ProductType.java
+++ src/main/java/com/real_estate/llk_server_spring/Product/Entity/ProductType.java
@@ -1,20 +1,15 @@
 package com.real_estate.llk_server_spring.Product.Entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ProductType {
     APARTMENT("APARTMENT", "아파트"), HOUSE("HOUSE","주택"),
     COMMERCIAL_LAND("COMMERCIAL LAND", "상업용 토지"), ETC("ETC", "기타");
 
     private final String eng;
     private final String kor;
-
-    private ProductType(String eng, String kor) {
-        this.eng = eng;
-        this.kor = kor;
-    }
-    public String getEng() {
-        return eng;
-    }
-    public String getKor() {
-        return kor;
-    }
 }
