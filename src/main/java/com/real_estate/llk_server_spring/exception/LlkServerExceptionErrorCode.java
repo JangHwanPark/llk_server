b4ddfb492ea154eb47b4f9020b115039106f7626
namung08 <namung08@gmail.com>
2024-07-20 23:48:07 +0900

    perf: Add server error code

--- src/main/java/com/real_estate/llk_server_spring/exception/LlkServerExceptionErrorCode.java
+++ src/main/java/com/real_estate/llk_server_spring/exception/LlkServerExceptionErrorCode.java
@@ -8,10 +8,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @AllArgsConstructor
 public enum LlkServerExceptionErrorCode {
-    NOT_FOUNT_AGENT("001_LLK","LLK real estate agent information not found."),
-    NOT_FOUNT_REVIEW("002_LLK","LLK real estate review information not found."),
+    NOT_FOUND_AGENT("001_LLK","LLK real estate agent information not found."),
+    NOT_FOUND_REVIEW("002_LLK","LLK real estate review information not found."),
     DONT_REQUEST_DATA("003_LLK","LLK request data not found."),
-    FALE_IMG_CONVERT("004_LLK","LLK img file convert failed."),;
+    FAIL_IMG_CONVERT("004_LLK","LLK img file convert failed."),
+    REQUEST_VALUE_DATABASE_DUPLICATE("005_LLK","LLK request value database duplicate."),
+    NOT_FOUND_STATE("006_LLK","LLK state data not found."),;
 
     private String errorCode;
     private String errorMessage;
